8cce6bd4b8a8eada4b2b69fe2d86bedf71f18d01
Drone 6969 <210758177+Drone-6969@users.noreply.github.com>
2025-12-01 00:13:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ⬡-Protocol
+
+⬡-Protocol implementation for Foundry VTT.
+
+## License
+
+This system is available under the [MIT License](https://opensource.org/license/MIT). See [LICENSE.md](https://github.com/Drone-6969/hexprotocol-fvtt/blob/main/LICENSE.md) for the full license text.
---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+
+// Add globals here so eslint doesn't yell at us for using them.
+// NOTE: Prefer namespaced paths over deprecated globals.
+const FOUNDRY_GLOBALS = {
+  CONFIG: "readonly",
+  CONST: "readonly",
+  foundry: "readonly",
+  game: "readonly",
+};
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...FOUNDRY_GLOBALS } },
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+  globalIgnores(["foundry/**/*"]),
+  eslintConfigPrettier,
+]);
